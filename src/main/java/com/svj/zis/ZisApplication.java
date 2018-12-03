8abce4a1cf4208daf1cc257d05656edde633d213
Jovo <sunjkajovo@gmail.com>
2018-12-03 18:32:25 +0100

    Initial project

---
+++ src/main/java/com/svj/zis/ZisApplication.java
@@ -0,0 +1,12 @@
+package com.svj.zis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZisApplication.class, args);
+	}
+}
